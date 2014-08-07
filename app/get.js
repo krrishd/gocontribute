d6213f497853dc2bb57ef0aceb2d9d688ea7af44
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-08-07 14:16:39 -0600

    Made it 1000 results

--- app/get.js
+++ app/get.js
@@ -5,8 +5,8 @@ function getRepoByLang(language, callback) {
 	var raw_data = [];
 	var index = 1;
 	function getData() {
-		if(index < 6) {
-			needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&per_page=100?page=' + index, function(err, res) {
+		if(index < 11) {
+			needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&page=' + index + '&per_page=100', function(err, res) {
 				raw_data.push(res.body);
 				console.log('Getting page ' + index + ' of search result');
 				index++;
@@ -16,6 +16,7 @@ function getRepoByLang(language, callback) {
 			return callback(raw_data);
 		}
 	}
+	getData();
 }
 
 module.exports = getRepoByLang;
