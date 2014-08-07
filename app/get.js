236ba1efeb3821abb5f141cacd85f1bfcf162592
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-08-07 13:52:48 -0600

    Making it load top 500 instead of top 100

--- app/get.js
+++ app/get.js
@@ -3,10 +3,16 @@ var needle = require('needle');
 
 function getRepoByLang(language, callback) {
 	var raw_data = [];
-	needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&per_page=100', function(err, res) {
-		raw_data.push(res.body);
+	var index = 1;
+	if(index < 6) {
+		needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&per_page=100?page=' + index, function(err, res) {
+			raw_data.push(res.body);
+			console.log('Getting page ' + index + ' of search result');
+			index++;
+		});
+	} else {
 		callback(raw_data);
-	});
+	}
 }
 
 module.exports = getRepoByLang;
