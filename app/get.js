04b4dddf26ed28ae1ed1bb963c14eb48c133062a
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-08-07 13:56:30 -0600

    Forgot something crucial, added recursion

--- app/get.js
+++ app/get.js
@@ -4,14 +4,17 @@ var needle = require('needle');
 function getRepoByLang(language, callback) {
 	var raw_data = [];
 	var index = 1;
-	if(index < 6) {
-		needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&per_page=100?page=' + index, function(err, res) {
-			raw_data.push(res.body);
-			console.log('Getting page ' + index + ' of search result');
-			index++;
-		});
-	} else {
-		callback(raw_data);
+	function getData() {
+		if(index < 6) {
+			needle.get('https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc&per_page=100?page=' + index, function(err, res) {
+				raw_data.push(res.body);
+				console.log('Getting page ' + index + ' of search result');
+				index++;
+				getData();
+			});
+		} else {
+			return callback(raw_data);
+		}
 	}
 }
 
